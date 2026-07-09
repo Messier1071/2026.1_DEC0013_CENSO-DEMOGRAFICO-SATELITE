49276c9209443b4b52907cc4de5daf057693adca
Mess <45535471+Messier1071@users.noreply.github.com>
2026-07-09 20:05:59 -0300

    .bat

---
+++ start.bat
@@ -0,0 +1,17 @@
+
+@echo off
+IF EXIST ".env" (
+    echo ".env exists, assuming setup is complete"
+    .\.venv\Scripts\activate
+    python src/main.py
+) ELSE (
+    echo File is missing.
+    python -m venv .venv
+    .\.venv\Scripts\activate
+    pip install -r .\scripts\requirements.txt
+    type nul > .env
+    echo GOOGLE_MAPS_API_KEY= >> .env
+    echo ROBOFLOW_API_KEY= >> .env
+)
+pause
+
